d843bc33c7bce265b3b25fd92cf02caccdc2da51
Kyoji OSADA <kyoji.osada@gmail.com>
2019-07-27 00:14:01 +0900

    initial commit

---
+++ js/auth.js
@@ -0,0 +1,1 @@
+var Act ={	_: function()	{		var query = $(location).attr('search');		var params = query.split('=');		if (params[1] === undefined || params[1] !== 'alt-stack') {			location.href = './unauth.html';		}	},};$(function(){	Act._();});
---
+++ js/custom.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  
+  // jquery.matchHeight
+  function MatchHeight() {
+    $('.faq-grid-question')
+      .matchHeight({})
+    ;
+  }
+	$(document).ready(function() {
+    MatchHeight(); 
+  });
+  
+  
+});
